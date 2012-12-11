efea27f871ee7948d9aa8d078b5b25cb8f67448d
Michal Petrucha <michal.petrucha@ksp.sk>
2012-12-11 19:54:22 +0100

    Added a ret instruction to each function definition.

--- c_llvm/ast/declarations.py
+++ c_llvm/ast/declarations.py
@@ -47,17 +47,33 @@ class FunctionDefinitionNode(AstNode):
 define %(type)s @%(name)s(%(args)s)
 {
 %(contents)s
+%(return)s
 }
+"""
+    ret_template = """
+%(register1)s = alloca %(type)s
+%(register2)s = load %(type)s* %(register1)s
+ret %(type)s %(register2)s
 """
 
     def generate_code(self, state):
         # TODO: Add the function to the symbol table.
         # TODO: Verify that the function isn't already defined.
+        type = state.types.get_type(str(self.return_type))
+        if type.internal_type == 'void':
+            ret_statement = "ret void"
+        else:
+            ret_statement = self.ret_template % {
+                'type': type.llvm_type,
+                'register1': state.get_tmp_register(),
+                'register2': state.get_tmp_register(),
+            }
         return self.template % {
-            'type': state.types.get_type(str(self.return_type)).llvm_type,
+            'type': type.llvm_type,
             'name': str(self.name),
             'args': '', # TODO: use the actual args
             'contents': self.body.generate_code(state),
+            'return': ret_statement,
         }
 
     def toString(self):
