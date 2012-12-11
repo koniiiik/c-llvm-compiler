614228415ad260b9bf6ff7f147df2a829e992640
Michal Petrucha <michal.petrucha@ksp.sk>
2012-12-11 05:31:47 +0100

    Fixed error handling in variable access.

--- c_llvm/ast/expressions.py
+++ c_llvm/ast/expressions.py
@@ -89,7 +89,8 @@ class VariableExpressionNode(ExpressionNode):
         try:
             var = state.symbols[str(self.name)]
         except KeyError:
-            self.log_error("unknown variable: %s" % (str(self.name),))
+            self.log_error(state, "unknown variable: %s" % (str(self.name),))
+            return ""
         register = state.get_tmp_register()
         state.last_expression_register = register
         return "%s = load %s* %s" % (register, var.type.llvm_type,
