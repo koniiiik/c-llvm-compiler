4166c2c1148a660d3a3b79550b6430eb846bf52e
Matúš Kukan <matus.kukan@gmail.com>
2012-12-21 02:08:17 +0100

    improve fibonacci program: break after 100 loops

--- test/fibonacci.c
+++ test/fibonacci.c
@@ -4,15 +4,17 @@ int scanf(char *format, ...),
 int main()
 {
     int a;
-    printf("type a number here: ");
-    scanf("%d", &a);
-    int f[10000000];
+    printf("enter a (positive) number: ");
+    scanf("%Ld", &a);
+    int f[1000];
     f[0] = 0;
     f[1] = 1;
     int i;
     for(i = 0; i < a; i++) {
         f[i+2] = f[i+1] + f[i];
         printf("%Ld\n", f[i+2]);
+        if (i > 100)
+            break;
     }
     return 0;
 }
